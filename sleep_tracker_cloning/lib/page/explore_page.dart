3c9818ce734b093d70e96d82f5064db131035e89
Muakjwa <booksunwoo@gmail.com>
2024-01-16 01:54:43 +0900

    240116

---
+++ sleep_tracker_cloning/lib/page/explore_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
+import 'package:vertical_scrollable_tabview/vertical_scrollable_tabview.dart';
+
+class explorePage extends StatefulWidget {
+  const explorePage({super.key});
+
+  @override
+  State<explorePage> createState() => _explorePageState();
+}
+
+class _explorePageState extends State<explorePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late AutoScrollController _autoScrollController;
+
+  static const List<Tab> myTabs = <Tab>[
+    Tab(text: '소리'),
+    Tab(text: '음악'),
+    Tab(text: '호흡'),
+    Tab(text: '스트레칭'),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _tabController = TabController(length: myTabs.length, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+        ),
+      ),
+    );
+  }
+}
--- sleep_tracker_cloning/lib/page/home.dart
+++ sleep_tracker_cloning/lib/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sleep_tracker_cloning/page/explore_page.dart';
 import 'package:sleep_tracker_cloning/page/sleep_page.dart';
 
 class Home extends StatefulWidget {
@@ -22,7 +23,7 @@ class _HomeState extends State<Home> {
         return sleepPage();
         break;
       case 1:
-        return Container();
+        return explorePage();
         break;
     }
     return Container(
